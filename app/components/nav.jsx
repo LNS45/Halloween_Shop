b8dc852aad33cfce2490594b0ee536c6a7dbcf4f
Lorenzo Noe Chavez Cerda <lorenzochavezxd@gmail.com>
2024-10-17 21:37:55 -0600

    Titulos agregados

---
+++ app/components/nav.jsx
@@ -0,0 +1,9 @@
+import "../globals.css"
+
+export default function Nav(){
+    return (
+        <nav className="w-10 h-10">
+            <h1>Hola</h1>
+        </nav>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-
+import fondo from "../app/images/BG.png"
+import "../app/globals.css"
+import Image from "next/image";
 
 export default function Home() {
   return (
-
+    <section className="relative z-10 flex flex-col h-screen gap-4 p-10">
+      <h1 className="relative z-10 text-6xl text-center text-naranja-halloween drop-shadow-lg md:text-7xl md:text-left md:w-3/4">¡Bienvenido a Rincón Espeluznante!</h1>
+      <h2 className="relative z-10 text-2xl text-center text-white drop-shadow-lg md:text-4xl md:text-left md:w-3/4">¡Haz que este Halloween sea inolvidable!</h2>
+      <Image src={fondo} fill={true} priority={true} className="absolute z-0 object-cover" alt="Fondo"/>
+    </section>
   );
 }
